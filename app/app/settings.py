9b6eb521509b6410a75f7d743e498ed89dfa26b2
sandeepparmar2k <sandeep.parmar2k@gmail.com>
2023-04-07 16:09:33 +0530

    Configured Django

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.back   ends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),  
