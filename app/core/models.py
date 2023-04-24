ee1425f47831eba241e4958615c9928b8b9e7fab
sandeepparmar2k <sandeep.parmar2k@gmail.com>
2023-04-24 09:19:33 +0530

    24apr

--- app/core/models.py
+++ app/core/models.py
@@ -7,12 +7,26 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin
 )
+
+
 class UserManager(BaseUserManager):
     """ Manager for users"""
 
-    def create_user(self, email, password=None, **extra_field):
-        user = self.model(email=email, **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        """ Create, save and return new password """
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """ create and return a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
